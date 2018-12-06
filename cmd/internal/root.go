439393c3f07356db8c64faf82fdc5449ae0e9b05
ENCALADA <encalada@de.ibm.com>
2018-12-06 22:03:10 +0100

    This commit covers two main changes
    1. Refactor the location of the `main.go` and cobra classes
       This include:
       - move `main.go` inside `cmd/cf-operator`, in order to follow
         generic project layout standards.
       - place the cobra classes `root.go` and `version.go` out of the
         `cmd` in favor of their own directory. Currently `cmd/internal`.
       - Refactor any reference towards the old dir locations.
    
    2. The `cfcontainerization/cf-operator` Dockerfile, can be build by
       using the multistage, where in the first stage we build the
       cf-operator binary, and then in the final stage we insert them.
       This should improve:
       - No need to run make/build before make/image.
       - In the future, if more golang repos are needed inside the image,
         it should improve the building image time.

---
+++ cmd/internal/root.go
@@ -0,0 +1,97 @@
+package cmd
+
+import (
+	"fmt"
+	golog "log"
+	"os"
+	"os/user"
+	"path/filepath"
+
+	"code.cloudfoundry.org/cf-operator/pkg/kube/operator"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc" // from https://github.com/kubernetes/client-go/issues/345
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+)
+
+var (
+	log *zap.SugaredLogger
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "cf-operator",
+	Short: "cf-operator manages BOSH deployments on Kubernetes",
+	Run: func(cmd *cobra.Command, args []string) {
+		defer log.Sync()
+
+		kubeConfig, err := getKubeConfig()
+		if err != nil {
+			log.Fatal(err)
+		}
+		namespace := viper.GetString("namespace")
+
+		log.Infof("Starting cf-operator with namespace %s", namespace)
+
+		mgr, err := operator.NewManager(log, kubeConfig, manager.Options{Namespace: namespace})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Fatal(mgr.Start(signals.SetupSignalHandler()))
+	},
+}
+
+// Execute the root command, runs the server
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		golog.Fatal(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringP("kubeconfig", "c", "", "Path to a kubeconfig, not required in-cluster")
+	rootCmd.PersistentFlags().StringP("namespace", "n", "default", "Namespace to watch for BOSH deployments")
+	viper.BindPFlag("kubeconfig", rootCmd.PersistentFlags().Lookup("kubeconfig"))
+	viper.BindPFlag("namespace", rootCmd.PersistentFlags().Lookup("namespace"))
+	viper.BindEnv("kubeconfig")
+	viper.BindEnv("namespace", "CFO_NAMESPACE")
+}
+
+// initConfig is executed before running commands
+func initConfig() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		golog.Fatalf("cannot initialize ZAP logger: %v", err)
+	}
+	log = logger.Sugar()
+}
+
+func getKubeConfig() (*rest.Config, error) {
+	kubeconfig := viper.GetString("kubeconfig")
+
+	if len(kubeconfig) > 0 {
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+
+	// If no explicit location, try the in-cluster config
+	if c, err := rest.InClusterConfig(); err == nil {
+		return c, nil
+	}
+
+	// If no in-cluster config, try the default location in the user's home directory
+	if usr, err := user.Current(); err == nil {
+		if c, err := clientcmd.BuildConfigFromFlags(
+			"", filepath.Join(usr.HomeDir, ".kube", "config")); err == nil {
+			return c, nil
+		}
+	}
+
+	return nil, fmt.Errorf("could not locate a kubeconfig")
+}
