2aa9ed0a2784a072f3eeafbc6c57e20b23e2280f
ENCALADA <encalada@de.ibm.com>
2019-07-04 14:25:24 +0200

    Separate storage tests in integration-tests
    - Add a common Make target for this, called test-storage
    - Add a new dir under integration, called storage
    - Add a new suite, for the storage_test package
    - Move existing integration-tests for pvcs into the new package

--- integration/suite_test.go
+++ integration/suite_test.go
@@ -2,13 +2,11 @@ package integration_test
 
 import (
 	"fmt"
-	"os/exec"
 	"testing"
 
 	"code.cloudfoundry.org/cf-operator/integration/environment"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/pkg/errors"
 )
 
 func TestIntegration(t *testing.T) {
@@ -34,29 +32,9 @@ var _ = AfterEach(func() {
 var _ = AfterSuite(func() {
 	// Nuking all namespaces at the end of the run
 	for _, namespace := range namespacesToNuke {
-		err := DeleteNamespace(namespace)
+		err := environment.DeleteNamespace(namespace, kubeCtlCmd)
 		if err != nil {
 			fmt.Printf("WARNING: failed to delete namespace %s: %v\n", namespace, err)
 		}
 	}
 })
-
-func DeleteNamespace(ns string) error {
-	fmt.Printf("Cleaning up namespace %s \n", ns)
-
-	_, err := RunBinary(kubeCtlCmd, "delete", "--ignore-not-found", "namespace", ns)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func RunBinary(binaryName string, args ...string) ([]byte, error) {
-	cmd := exec.Command(binaryName, args...)
-	stdOutput, err := cmd.CombinedOutput()
-	if err != nil {
-		return stdOutput, errors.Wrapf(err, "%s cmd, failed with the following error: %s", cmd.Args, string(stdOutput))
-	}
-	return stdOutput, nil
-}
