7b61be84b81f93c03e0fc522e20dcf766b8b3b3b
Sakala Venkata Krishna Rohit <rohitsakala@gmail.com>
2020-05-26 11:42:42 +0530

    Add e2e tests

---
+++ e2e/cli/suite_test.go
@@ -0,0 +1,32 @@
+package cli_test
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+
+	"testing"
+)
+
+func TestE2ECli(t *testing.T) {
+	SetDefaultEventuallyTimeout(10 * time.Second)
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "E2E CLI Suite")
+}
+
+var (
+	cliPath string
+)
+
+var _ = BeforeSuite(func() {
+	var err error
+	cliPath, err = gexec.Build("code.cloudfoundry.org/quarks-secret/cmd")
+	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	gexec.KillAndWait()
+	gexec.CleanupBuildArtifacts()
+})
