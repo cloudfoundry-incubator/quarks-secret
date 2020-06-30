7e0413e99e663953205dd40d896343737117129b
Mario Manno <mario.manno@suse.com>
2020-06-30 12:52:08 +0200

    Remove docker image arg
    was not used

--- integration/environment/operator.go
+++ integration/environment/operator.go
@@ -1,14 +1,10 @@
 package environment
 
 import (
-	"os"
-
-	"github.com/pkg/errors"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc" //from https://github.com/kubernetes/client-go/issues/345
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	"code.cloudfoundry.org/quarks-secret/pkg/kube/operator"
-	sharedcfg "code.cloudfoundry.org/quarks-utils/pkg/config"
 )
 
 // StartOperator starts the quarks secret operator
@@ -29,26 +25,6 @@ func (e *Environment) StartOperator() (chan struct{}, error) {
 
 func (e *Environment) setupOperator() (manager.Manager, error) {
 	ctx := e.SetupLoggerContext("quarks-secret-tests")
-
-	dockerImageOrg, found := os.LookupEnv("DOCKER_IMAGE_ORG")
-	if !found {
-		dockerImageOrg = "cfcontainerization"
-	}
-
-	dockerImageRepo, found := os.LookupEnv("DOCKER_IMAGE_REPOSITORY")
-	if !found {
-		dockerImageRepo = "quarks-secret"
-	}
-
-	dockerImageTag, found := os.LookupEnv("DOCKER_IMAGE_TAG")
-	if !found {
-		return nil, errors.Errorf("required environment variable DOCKER_IMAGE_TAG not set")
-	}
-
-	err := sharedcfg.SetupOperatorDockerImage(dockerImageOrg, dockerImageRepo, dockerImageTag)
-	if err != nil {
-		return nil, err
-	}
 
 	mgr, err := operator.NewManager(ctx, e.Config, e.KubeConfig, manager.Options{
 		MetricsBindAddress: "0",
