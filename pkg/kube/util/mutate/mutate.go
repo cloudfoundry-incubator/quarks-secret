a8bf27794e8eee38ad8932b37bb7eb5769747281
Sakala Venkata Krishna Rohit <rohitsakala@gmail.com>
2020-07-09 07:54:24 +0200

    debug

--- pkg/kube/util/mutate/mutate.go
+++ pkg/kube/util/mutate/mutate.go
@@ -1,9 +1,11 @@
 package mutate
 
 import (
+	"fmt"
 	"reflect"
 
 	qsv1a1 "code.cloudfoundry.org/quarks-secret/pkg/kube/apis/quarkssecret/v1alpha1"
+	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
@@ -14,11 +16,33 @@ import (
 func QuarksSecretMutateFn(qSec *qsv1a1.QuarksSecret) controllerutil.MutateFn {
 	updated := qSec.DeepCopy()
 	return func() error {
-		qSec.Labels = updated.Labels
-		qSec.Annotations = updated.Annotations
-		qSec.Spec = updated.Spec
+		changed := false
+		if !reflect.DeepEqual(qSec.Labels, updated.Labels) {
+			fmt.Println("Old Labels", qSec.Labels)
+			fmt.Println("New Labels", updated.Labels)
+			qSec.Labels = updated.Labels
+			changed = true
+		}
 
-		return nil
+		if !reflect.DeepEqual(qSec.Annotations, updated.Annotations) {
+			fmt.Println("Old Annotations", qSec.Annotations)
+			fmt.Println("New Annotations", updated.Annotations)
+			qSec.Annotations = updated.Annotations
+			changed = true
+		}
+
+		if !reflect.DeepEqual(qSec.Spec, updated.Spec) {
+			fmt.Println("Old Spec", qSec.Spec)
+			fmt.Println("New Spec", updated.Spec)
+			qSec.Spec = updated.Spec
+			changed = true
+		}
+
+		if changed {
+			return nil
+		}
+
+		return errors.New("Nothing updated")
 	}
 }
 
