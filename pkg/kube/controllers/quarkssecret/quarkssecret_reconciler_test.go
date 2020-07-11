824a7d4e4dee67c66ffbbd4cd1e3e7194bdf1b68
Edward <shawzida@gmail.com>
2020-07-11 16:22:34 +0800

    Add e2e case for dockerConfigJson secret

--- pkg/kube/controllers/quarkssecret/quarkssecret_reconciler_test.go
+++ pkg/kube/controllers/quarkssecret/quarkssecret_reconciler_test.go
@@ -209,7 +209,7 @@ var _ = Describe("ReconcileQuarksSecret", func() {
 
 	Context("when generating dockerConfigJson secret", func() {
 		BeforeEach(func() {
-			qSecret.Spec.Type = qsv1a1.DockerConfigJson
+			qSecret.Spec.Type = qsv1a1.DockerConfigJSON
 
 			qSecret.Spec.ImageCredentials = qsv1a1.ImageCredentials{
 				Registry: "fake.registry",
