06cda22472b0ac9923bc81747b1ab1aa43eaaa53
Sakala Venkata Krishna Rohit <rohitsakala@gmail.com>
2020-08-04 15:56:19 +0200

    Add quarks secret labels (#25)
    Co-authored-by: Cibaca Khandelwal <khandelwalcibaca@gmail.com>

--- pkg/kube/apis/quarkssecret/v1alpha1/zz_generated.deepcopy.go
+++ pkg/kube/apis/quarkssecret/v1alpha1/zz_generated.deepcopy.go
@@ -86,6 +86,13 @@ func (in *QuarksSecret) DeepCopyInto(out *QuarksSecret) {
 	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
 	in.Spec.DeepCopyInto(&out.Spec)
 	in.Status.DeepCopyInto(&out.Status)
+	if in.SecretLabels != nil {
+		in, out := &in.SecretLabels, &out.SecretLabels
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
 	return
 }
 
