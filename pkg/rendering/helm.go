a169adde28d268f2a7fc925bf7a0c98fd4b6aae4
Ettore Di Giacinto <edigiacinto@suse.com>
2020-08-28 14:43:37 +0200

    Add templatedConfig type

--- pkg/rendering/helm.go
+++ pkg/rendering/helm.go
@@ -8,21 +8,36 @@ import (
 	"helm.sh/helm/v3/pkg/engine"
 )
 
+// HelmEngine is the helm engine constant to identify the engine type
+const HelmEngine = "helm"
+
+// HelmRenderingEngine is a rendering engine based on helm which 
+// satisfies the RenderingEngine interface
 type HelmRenderingEngine struct{}
 
+// NewHelmRenderingEngine returns a new Helm engine
 func NewHelmRenderingEngine() RenderingEngine {
 	return HelmRenderingEngine{}
 }
 
+// Render renders the template interpolating the supplied values
 func (h HelmRenderingEngine) Render(content string, values map[string]interface{}) string {
-	out := h.RenderFiles([]*chart.File{
+	out := h.renderFiles([]*chart.File{
 		{Name: "templates", Data: []byte(content)},
 	}, map[string]interface{}{}, values)
 
 	return out["templates"]
 }
 
-func (h HelmRenderingEngine) RenderFiles(files []*chart.File, values map[string]interface{}, defaults map[string]interface{}) map[string]string {
+// RenderMap renders the values passed from a map[string]string
+func (h HelmRenderingEngine) RenderMap(contentMap map[string]string, values map[string]interface{}) map[string]string {
+	for k, v := range contentMap {
+		contentMap[k] = h.Render(v, values)
+	}
+	return contentMap
+}
+
+func (h HelmRenderingEngine) renderFiles(files []*chart.File, values map[string]interface{}, defaults map[string]interface{}) map[string]string {
 	c := &chart.Chart{
 		Metadata: &chart.Metadata{
 			Name:    "",
