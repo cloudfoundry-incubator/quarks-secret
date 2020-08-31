a3f975bc4f5b4b4e57ab1fc6cc4ef8f73e44a379
Mario Manno <mario.manno@suse.com>
2020-08-31 15:19:23 +0200

    Remove interface and make funcs private

--- pkg/rendering/helm.go
+++ pkg/rendering/helm.go
@@ -11,34 +11,33 @@ import (
 // HelmEngine is the helm engine constant to identify the engine type
 const HelmEngine = "helm"
 
-// HelmRenderingEngine is a rendering engine based on helm which
-// satisfies the RenderingEngine interface
+// HelmRenderingEngine is a rendering engine based on helm
 type HelmRenderingEngine struct{}
 
 // NewHelmRenderingEngine returns a new Helm engine
-func NewHelmRenderingEngine() Engine {
+func NewHelmRenderingEngine() HelmRenderingEngine {
 	return HelmRenderingEngine{}
 }
 
-// Render renders the template interpolating the supplied values
-func (h HelmRenderingEngine) Render(content string, values map[string]interface{}) string {
-	out := h.RenderFiles([]*chart.File{
+// RenderMap renders the values passed from a map[string]string
+func (h HelmRenderingEngine) RenderMap(contentMap map[string]string, values map[string]interface{}) map[string]string {
+	for k, v := range contentMap {
+		contentMap[k] = h.render(v, values)
+	}
+	return contentMap
+}
+
+// render renders the template interpolating the supplied values
+func (h HelmRenderingEngine) render(content string, values map[string]interface{}) string {
+	out := h.renderFiles([]*chart.File{
 		{Name: "templates", Data: []byte(content)},
 	}, map[string]interface{}{}, values)
 
 	return out["templates"]
 }
 
-// RenderMap renders the values passed from a map[string]string
-func (h HelmRenderingEngine) RenderMap(contentMap map[string]string, values map[string]interface{}) map[string]string {
-	for k, v := range contentMap {
-		contentMap[k] = h.Render(v, values)
-	}
-	return contentMap
-}
-
-// RenderFiles render files as they where charts with helm
-func (h HelmRenderingEngine) RenderFiles(files []*chart.File, values map[string]interface{}, defaults map[string]interface{}) map[string]string {
+// renderFiles render files as they where charts with helm
+func (h HelmRenderingEngine) renderFiles(files []*chart.File, values map[string]interface{}, defaults map[string]interface{}) map[string]string {
 	c := &chart.Chart{
 		Metadata: &chart.Metadata{
 			Name:    "",
--- pkg/rendering/render.go
+++
@@ -1,8 +0,0 @@
-package rendering
-
-// Engine is an interface representing
-// a generic rendering engine
-type Engine interface {
-	Render(content string, values map[string]interface{}) string
-	RenderMap(contentMap map[string]string, values map[string]interface{}) map[string]string
-}
