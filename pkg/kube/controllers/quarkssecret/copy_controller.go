42fbe352e212092e5c6862ba5ff3c239018022bb
Sakala Venkata Krishna Rohit <rohitsakala@gmail.com>
2020-09-08 17:37:40 +0200

    Add copy controller

---
+++ pkg/kube/controllers/quarkssecret/copy_controller.go
@@ -0,0 +1,173 @@
+package quarkssecret
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	crc "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	credsgen "code.cloudfoundry.org/quarks-secret/pkg/credsgen/in_memory_generator"
+	qsv1a1 "code.cloudfoundry.org/quarks-secret/pkg/kube/apis/quarkssecret/v1alpha1"
+	"code.cloudfoundry.org/quarks-utils/pkg/config"
+	"code.cloudfoundry.org/quarks-utils/pkg/ctxlog"
+	"code.cloudfoundry.org/quarks-utils/pkg/skip"
+)
+
+// AddCopy creates a new QuarksSecrets controller to watch for the
+// user defined secrets.
+func AddCopy(ctx context.Context, config *config.Config, mgr manager.Manager) error {
+	ctx = ctxlog.NewContextWithRecorder(ctx, "copy-reconciler", mgr.GetEventRecorderFor("copy-recorder"))
+	log := ctxlog.ExtractLogger(ctx)
+	r := NewCopyReconciler(ctx, config, mgr, credsgen.NewInMemoryGenerator(log), controllerutil.SetControllerReference)
+
+	c, err := controller.New("copy-controller", mgr, controller.Options{
+		Reconciler:              r,
+		MaxConcurrentReconciles: config.MaxQuarksSecretWorkers,
+	})
+	if err != nil {
+		return errors.Wrap(err, "Adding copy controller to manager failed.")
+	}
+
+	nsPred := newNSPredicate(ctx, mgr.GetClient(), config.MonitoredID)
+
+	// Watch for changes to the copied status of QuarksSecrets
+	p := predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool { return false }
+		DeleteFunc:  func(e event.DeleteEvent) bool { return false },
+		GenericFunc: func(e event.GenericEvent) bool { return false },
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			n := e.ObjectNew.(*qsv1a1.QuarksSecret)
+			o := e.ObjectOld.(*qsv1a1.QuarksSecret)
+
+			return *n.
+
+			
+		},
+	}
+	err = c.Watch(&source.Kind{Type: &qsv1a1.QuarksSecret{}}, &handler.EnqueueRequestForObject{}, nsPred, p)
+	if err != nil {
+		return errors.Wrapf(err, "Watching quarks secrets failed in quarksSecret controller.")
+	}
+
+	// Watch for changes to user created secrets
+	p = predicate.Funcs{
+		CreateFunc:  func(e event.CreateEvent) bool { return false },
+		DeleteFunc:  func(e event.DeleteEvent) bool { return false },
+		GenericFunc: func(e event.GenericEvent) bool { return false },
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			n := e.ObjectNew.(*corev1.Secret)
+			o := e.ObjectOld.(*corev1.Secret)
+
+			shouldProcessReconcile := isUserCreatedSecret(n)
+			if reflect.DeepEqual(n.Data, o.Data) && reflect.DeepEqual(n.Labels, o.Labels) &&
+				reflect.DeepEqual(n.Annotations, o.Annotations) {
+				return false
+			}
+
+			return shouldProcessReconcile
+		},
+	}
+	err = c.Watch(&source.Kind{Type: &corev1.Secret{}}, &handler.EnqueueRequestsFromMapFunc{
+		ToRequests: handler.ToRequestsFunc(func(a handler.MapObject) []reconcile.Request {
+			secret := a.Object.(*corev1.Secret)
+
+			if skip.Reconciles(ctx, mgr.GetClient(), secret) {
+				return []reconcile.Request{}
+			}
+
+			reconciles, err := listQuarksSecretsReconciles(ctx, mgr.GetClient(), secret, secret.Namespace)
+			if err != nil {
+				ctxlog.Errorf(ctx, "Failed to calculate reconciles for secret '%s/%s': %v", secret.Namespace, secret.Name, err)
+			}
+			if len(reconciles) > 0 {
+				return reconciles
+			}
+
+			return reconciles
+		}),
+	}, nsPred, p)
+	if err != nil {
+		return errors.Wrapf(err, "Watching secrets failed in quarks secret controller.")
+	}
+
+	return nil
+}
+
+// listSecrets gets all Secrets owned by the QuarksSecret
+func listSecrets(ctx context.Context, client crc.Client, qsec *qsv1a1.QuarksSecret) ([]corev1.Secret, error) {
+	ctxlog.Debug(ctx, "Listing Secrets owned by QuarksSecret '", qsec.GetNamespacedName(), "'")
+
+	secretLabels := map[string]string{qsv1a1.LabelKind: qsv1a1.GeneratedSecretKind}
+	result := []corev1.Secret{}
+
+	allSecrets := &corev1.SecretList{}
+	err := client.List(ctx, allSecrets,
+		crc.InNamespace(qsec.Namespace),
+		crc.MatchingLabels(secretLabels),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, s := range allSecrets.Items {
+		secret := s
+		if metav1.IsControlledBy(&secret, qsec) {
+			result = append(result, secret)
+			ctxlog.Debug(ctx, "Found Secret '", secret.Name, "' owned by QuarksSecret '", qsec.GetNamespacedName(), "'")
+		}
+	}
+
+	if len(result) == 0 {
+		ctxlog.Debug(ctx, "Did not find any Secret owned by QuarksSecret '", qsec.GetNamespacedName(), "'")
+	}
+
+	return result, nil
+}
+
+func isUserCreatedSecret(secret *corev1.Secret) bool {
+	secretLabels := secret.GetLabels()
+	value, ok := secretLabels[qsv1a1.LabelKind]
+	if !ok {
+		return true
+	}
+	if value != qsv1a1.GeneratedSecretKind {
+		return true
+	}
+	return !ok
+}
+
+// listQuarksSecretsReconciles lists all Quarks Secrets associated with the a particular secret.
+func listQuarksSecretsReconciles(ctx context.Context, client crc.Client, secret *corev1.Secret, namespace string) ([]reconcile.Request, error) {
+	quarksSecretList := &qsv1a1.QuarksSecretList{}
+	err := client.List(ctx, quarksSecretList, crc.InNamespace(namespace))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to list QuarksSecrets")
+	}
+
+	result := []reconcile.Request{}
+	for _, quarksSecret := range quarksSecretList.Items {
+		if quarksSecret.Spec.SecretName == secret.Name {
+			request := reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      quarksSecret.Name,
+					Namespace: quarksSecret.Namespace,
+				}}
+			result = append(result, request)
+			ctxlog.NewMappingEvent(secret).Debug(ctx, request, "QuarksSecret", secret.Name, qsv1a1.KubeSecretReference)
+		}
+	}
+	return result, nil
+}
