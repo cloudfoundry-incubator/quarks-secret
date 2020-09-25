1ca5cd738cfeb17b94b62d36b863f94c74528e20
Mario Manno <mario.manno@suse.com>
2020-09-25 14:34:15 +0200

    Only trigger secret updates for changes to the qsec spec
    Don't trigger for label
    
    [#174985178](https://www.pivotaltracker.com/story/show/174985178)

--- integration/quarks_secret_test.go
+++ integration/quarks_secret_test.go
@@ -1,6 +1,9 @@
 package integration_test
 
 import (
+	"strconv"
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
@@ -19,12 +22,12 @@ var _ = Describe("QuarksSecret", func() {
 
 	JustBeforeEach(func() {
 		By("Creating the quarks secret", func() {
-			qs, tearDown, err := env.CreateQuarksSecret(env.Namespace, qs)
+			qsec, tearDown, err := env.CreateQuarksSecret(env.Namespace, qs)
+			qs = *qsec
 			Expect(err).NotTo(HaveOccurred())
 			Expect(qs).NotTo(Equal(nil))
 			tearDowns = append(tearDowns, tearDown)
 		})
-
 	})
 
 	AfterEach(func() {
@@ -150,8 +153,10 @@ var _ = Describe("QuarksSecret", func() {
 			deleteQuarksSecret()
 
 			By("creating an new cluster ca qsec")
-			qs.Spec.Request.CertificateRequest.AlternativeNames = []string{"qux.com", "example.org"}
-			_, _, err = env.CreateQuarksSecret(env.Namespace, qs)
+			qsnew := env.CertificateQuarksSecret(qsName, "mysecret", "ca", "key")
+			qsnew.Spec.Request.CertificateRequest.SignerType = qsv1a1.ClusterSigner
+			qsnew.Spec.Request.CertificateRequest.AlternativeNames = []string{"qux.com", "example.org"}
+			_, _, err = env.CreateQuarksSecret(env.Namespace, qsnew)
 			Expect(err).NotTo(HaveOccurred())
 
 			By("checking for a working generated secret")
@@ -180,4 +185,72 @@ var _ = Describe("QuarksSecret", func() {
 		})
 	})
 
+	When("updating the qsec", func() {
+		var oldSecret *corev1.Secret
+
+		BeforeEach(func() {
+			qs = env.DefaultQuarksSecret(qsName)
+			secretName = qs.Spec.SecretName
+		})
+
+		JustBeforeEach(func() {
+			var err error
+			oldSecret, err = env.CollectSecret(env.Namespace, secretName)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Getting the current version")
+			qs, err = env.GetQuarksSecret(env.Namespace, qs.Name)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		incResourceVersion := func(v string) string {
+			n, err := strconv.Atoi(v)
+			Expect(err).NotTo(HaveOccurred())
+			n++
+			return strconv.Itoa(n)
+		}
+
+		When("updating the spec", func() {
+			JustBeforeEach(func() {
+				var err error
+				By("Updating the quarks secret")
+				qs.Spec.SecretLabels = map[string]string{"sec": "label"}
+				qs, err = env.UpdateQuarksSecret(env.Namespace, qs)
+				Expect(err).NotTo(HaveOccurred())
+
+				old := incResourceVersion(qs.GetResourceVersion())
+				err = env.WaitForQuarksSecretChange(env.Namespace, qs.Name, 10*time.Second, func(qsec qsv1a1.QuarksSecret) bool {
+					return old < qsec.GetResourceVersion()
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+			})
+
+			It("updates the secret", func() {
+				secret, err := env.CollectSecret(env.Namespace, secretName)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(secret.Data["password"]).NotTo(Equal(oldSecret.Data["password"]))
+				qs, err = env.GetQuarksSecret(env.Namespace, qs.Name)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+
+		When("updating a label", func() {
+			JustBeforeEach(func() {
+				var err error
+				qs.Labels = map[string]string{"qsec": "label"}
+				qs, err = env.UpdateQuarksSecret(env.Namespace, qs)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("does not update the secret", func() {
+				old := incResourceVersion(qs.GetResourceVersion())
+				err := env.WaitForQuarksSecretChange(env.Namespace, qs.Name, 10*time.Second, func(qsec qsv1a1.QuarksSecret) bool {
+					return old < qsec.GetResourceVersion()
+				})
+				Expect(err).To(HaveOccurred(), "check for update should fail")
+			})
+		})
+	})
 })
